59cbb34001c5a3dda2866d64211d8c3e0708d3f3
TanTanmayMay <severscanorsingularity@gmail.com>
2024-07-27 19:17:17 +0530

    Added the UTS namespace to new Process using SysProcAttr control structure

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
@@ -18,12 +19,16 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
+	// cmd is typically a variable representing an instance of exec.Cmd, which is used to execute external commands.
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
 	
-	cmd.Run()
+	checkErr(cmd.Run())
 }
 
 func checkErr(err error) {
